e2ca4d0539b35d93b28b25d5c280d8875cc23174
Rookev <kevin.roos@web.de>
2020-10-10 23:45:08 +0200

    Restructure AppContent with component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Setlist from './Setlist.js';
-import Cardlist from './Cardlist.js';
+import AppContent from './AppContent.js';
 import './App.css';
 
 function displaySets() {
-  const setlist = <Setlist></Setlist>
-  ReactDOM.render(setlist, document.getElementById('App-sets'));
-
-  const cardlist = <Cardlist></Cardlist>
-  ReactDOM.render(cardlist, document.getElementById('App-cards'));
+  const appContent = <AppContent></AppContent>;
+  ReactDOM.render(appContent, document.getElementById('App-content'));
 }
 
 function App() {
@@ -17,13 +13,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <button value="Display Sets"
-          onClick={displaySets}
+          onClick={displayAppContent}
         >Display Sets</button>
       </header>
-      <div id="App-content" className="App-content">
-        <div id="App-sets" className="App-sets"></div>
-        <div id="App-cards" className="App-cards"></div>
-      </div>
+      <div id="App-content" className="App-content"></div>
     </div>
   );
 }
---
+++ src/AppContent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Setlist from './Setlist.js';
+import Cardlist from './Cardlist.js';
+
+class AppContent extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="App-content" className="App-content">
+        <div id="App-sets" className="App-sets">
+          <Setlist></Setlist>
+        </div>
+
+        <div id="App-cards" className="App-cards">
+          <Cardlist></Cardlist>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AppContent;
