3a50969d3f22847aec2e74da28d0249d3736ebdb
Rookev <kevin.roos@web.de>
2020-10-11 11:18:07 +0200

    Replace button with Material button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContent from './AppContent.js';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function displayAppContent() {
   const appContent = <AppContent></AppContent>;
@@ -14,9 +15,9 @@ function App() {
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       <header className="App-header">
-        <button value="Display Sets"
-          onClick={displayAppContent}
-        >Display Sets</button>
+        <Button variant="contained" color="primary" onClick={displayAppContent}>
+          Display Sets
+        </Button>
       </header>
       <div id="App-content" className="App-content"></div>
     </div>
