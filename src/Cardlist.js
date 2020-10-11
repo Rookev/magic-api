6e16df65a7d05d2d4bb4bf3de889ea37770d9522
Rookev <kevin.roos@web.de>
2020-10-11 10:52:21 +0200

    Implement card buffer

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
 class Cardlist extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      cards: undefined
+      cards: {}
     };
   }
 
@@ -12,65 +13,41 @@ class Cardlist extends Component {
     fetch("https://api.scryfall.com/cards/search?order=set&q=set:" + set)
       .then(res => res.json())
       .then((result) => {
+        var updatedCardBuffer = this.state.cards;
+        updatedCardBuffer[set] = result.data;
         this.setState({
-          cards: result.data
+          cards: updatedCardBuffer
         });
-      },
-        (error) => {
-        })
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps.set !== this.props.set) {
-      return true;
-    }
-
-    if (nextState.cards === undefined) {
-      return false;
-    }
-
-    return true;
+      })
   }
 
   render() {
-    // Initial rendering without set
-    if (!this.props.set) {
-      return <h1>I am an unloaded Cardlist :-(</h1>;
+    // No buffered cards found -> Start async load (function loadCards) and return loading message
+    if (!this.state.cards[this.props.set]) {
+      this.loadCards(this.props.set);
+      return <h1>Loading Set: {this.props.set}...</h1>;
     }
 
-    // A set prop has been specified
+    // Loaded
     else {
-      // Not yet loaded -> Start async load (function loadCards) and return loading message
-      if (!this.state.cards) {
-        this.loadCards(this.props.set);
-        return <h1>Loading Set: {this.props.set}...</h1>;
-      }
+      var aCardsWithImageUris = this.state.cards[this.props.set].filter(function (oCard) {
+        return (oCard && oCard.image_uris && oCard.image_uris.normal)
+      });
 
-      // Loaded
-      else {
-        var aCardsWithImageUris = this.state.cards.filter(function (oCard) {
-          return (oCard && oCard.image_uris && oCard.image_uris.normal)
-        });
-
-        this.setState({
-          cards: undefined
-        });
-
-        return (
-          <div>
-            <h1>Set {this.props.set}: {aCardsWithImageUris.length} cards</h1>
-            <ul>
-              {aCardsWithImageUris.map((oCard) => {
-                return (
-                  <li key={oCard.id} className="Card">
-                    <img src={oCard.image_uris.normal} alt={oCard.name} />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        );
-      }
+      return (
+        <div>
+          <h1>Set {this.props.set}: {aCardsWithImageUris.length} cards</h1>
+          <ul>
+            {aCardsWithImageUris.map((oCard) => {
+              return (
+                <li key={oCard.id} className="Card">
+                  <img src={oCard.image_uris.normal} alt={oCard.name} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
     }
   }
 }
