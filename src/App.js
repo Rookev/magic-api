441de7c05aeba972a24231bd55f0fe512a4c34ed
Rookev <kevin.roos@web.de>
2020-10-11 12:53:49 +0200

    Replace header with AppBar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import AppContent from './AppContent.js';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 function displayAppContent() {
   const appContent = <AppContent></AppContent>;
@@ -16,11 +19,18 @@ function App() {
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       <Container maxWidth="100%">
-      <header className="App-header">
+      
+      <AppBar position="static">
+        <Toolbar>
         <Button variant="contained" color="primary" onClick={displayAppContent}>
           Display Sets
         </Button>
-      </header>
+  </Toolbar>
+</AppBar>
+
+
+
+
       <div id="App-content" className="App-content"></div>
       </Container>
     </div>
